64cf832dae206f66b833256e0c8c671f0ffc2b4c
Rocio-Aguirre <rocioag.6984@gmail.com>
2021-09-08 16:05:27 -0300

    Tarea 1 - resuelta

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,109 @@
+var CACHE_STATIC_NAME = 'static-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/css/main.css',
+          '/src/js/main.js',
+          '/src/js/material.min.js',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+      })
+  )
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
-  return self.clients.claim();
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME) {
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
+            })
+            .catch(function(err) {
+
+            });
+        }
+      })
+  );
+});
+//1) Cache with Network fallback.
+/*
+2) NETWORK ONLY STRATEGY
+self.addEventListener('fetch', event =>{
   event.respondWith(fetch(event.request));
 });
+*/
+/*
+3) CACHE ONLY STRATEGY
+self.addEventListener('fetch', event =>{
+  event.respondWith(caches.match(event.request));
+});
+*/
+/*
+4) NETWORK, CACHE FALLBACK STRATEGY
+self.addEventListener('fetch', event =>{
+  event.respondWith(
+  fetch(event.request).then(res =>{
+      return caches.open(ACHE_DYNAMIC_NAME).then((cache) => {
+         cache.put(event.request.url, res.clone());
+         return res;
+       })
+     })
+     .catch(() => {
+       return caches.match(event.request);
+     })
+    
+ )
+};
+*/
+/*
+5) CACHE, THEN NETWORK STRATEGY
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        } 
+          return fetch(event.request)
+            .then(res=> {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(cache => {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
+            })
+      })
+  );
+});
+
+*/
